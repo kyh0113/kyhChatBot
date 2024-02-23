207d935f4166d81babc62f1a43771efa9e08274d
kyh0113 <cin05189@gmail.com>
2024-02-23 20:35:01 +0900

    텔레그램 접속 시 메세지 알람

--- src/main/java/com/example/telegram.java
+++ src/main/java/com/example/telegram.java
@@ -9,21 +9,26 @@ public class telegram {
 	public static void funcTelegram(){
 		String Token = "7185061470:AAGJqrpH3PmLME3o6D_Zvbu8PCJFCILUO3s";
 		String chat_id = "5976131197";
-		String text = "메시지 전송 테스트" ;
-		
-		BufferedReader in = null;
-		
-		 try {
-			 URL obj = new URL("https://api.telegram.org/bot" + Token + "/sendmessage?chat_id=" + chat_id + "&text=" + text); // 호출할 url
-			 
-			 HttpURLConnection con = (HttpURLConnection)obj.openConnection();
-			 con.setRequestMethod("GET");
-			 in = new BufferedReader(new InputStreamReader(con.getInputStream(), "UTF-8"));
-			 String line;
-			 
-			 while((line = in.readLine()) != null) { // response를 차례대로 출력
-				 System.out.println(line);
-			 }		 
+		String text = "안녕? 만나서 반가워. \n 이건 나를 소개하기 위한 챗봇이야~ 즐겁게 나를 소개할게. \n 최종 시험을 통과하면 선물도 있어.";
+        
+        // 개행 문자를 기준으로 문자열을 나누어 배열에 저장
+        String[] lines = text.split("\n");
+        
+        BufferedReader in = null;
+        
+        try {
+            for (String line : lines) {
+                // 각 줄을 URL에 추가하여 호출
+                URL obj = new URL("https://api.telegram.org/bot" + Token + "/sendmessage?chat_id=" + chat_id + "&text=" + line);
+                HttpURLConnection con = (HttpURLConnection)obj.openConnection();
+                con.setRequestMethod("GET");
+                in = new BufferedReader(new InputStreamReader(con.getInputStream(), "UTF-8"));
+                String response;
+                
+                while((response = in.readLine()) != null) { // response를 차례대로 출력
+                    System.out.println(response);
+                }
+            }	 
 			 
 		 } catch(Exception e) {
 			 e.printStackTrace();
